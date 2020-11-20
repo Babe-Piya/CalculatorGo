d5046281bde38096512a85a21d9c9a62b46f7eb0
babe <piya.p@cdg.co.th>
2020-11-20 16:17:29 +0700

    edit

--- calculator.go
+++ calculator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 )
 
@@ -12,24 +13,45 @@ var result int64
 
 func main() {
 
-	fmt.Print("Please enter operator :  ")
-	fmt.Scan(&operator)
-	operator = operatorCheck(operator)
+	if len(os.Args) > 3 {
+		operator = operatorCheck(os.Args[1])
+		numFirst, err := operandCheck(os.Args[2])
+		if err != nil {
+			fmt.Println("Invalid number Please try again.")
+		}
+		numSecond, err := operandCheck(os.Args[3])
+		if err != nil {
+			fmt.Println("Invalid number Please try again.")
+		}
+		cal(operator, numFirst, numSecond)
+	}
+	for {
+		fmt.Print("Please enter operator :  ")
+		fmt.Scan(&operator)
+		operator = operatorCheck(operator)
 
-	fmt.Print("Please enter first number : ")
-	fmt.Scan(&first)
-	numFirst := operandCheck(first)
+		fmt.Print("Please enter first number : ")
+		fmt.Scan(&first)
+		numFirst, err := operandCheck(first)
+		if err != nil {
+			fmt.Println("Invalid number Please try again.")
+			continue
+		}
 
-	fmt.Print("Please enter second number :  ")
-	fmt.Scan(&second)
-	numSecond := operandCheck(second)
+		fmt.Print("Please enter second number :  ")
+		fmt.Scan(&second)
+		numSecond, err := operandCheck(second)
+		if err != nil {
+			fmt.Println("Invalid number Please try again.")
+			continue
+		}
 
-	result = cal(operator, numFirst, numSecond)
-	fmt.Printf("Result : %d", result)
+		cal(operator, numFirst, numSecond)
+	}
 
 }
 
-func cal(operator string, numFirst int64, numSecond int64) int64 {
+func cal(operator string, numFirst int64, numSecond int64) {
 	var result int64
 
 	switch operator {
@@ -42,10 +64,11 @@ func cal(operator string, numFirst int64, numSecond int64) int64 {
 	case "/":
 		result = numFirst / numSecond
 	}
-	return result
+	fmt.Printf("Result : %d\n", result)
 }
 
 func operatorCheck(operator string) string {
+
 	for i := 0; i < 1; {
 		switch operator {
 		case "+", "-", "*", "/":
@@ -59,23 +82,10 @@ func operatorCheck(operator string) string {
 	return operator
 }
 
-func operandCheck(num string) int64 {
-	var numChecked int64
-	for i := 0; i < 1; {
-		if isNumeric(num) {
-			numChecked, _ = strconv.ParseInt(num, 10, 64)
-			i++
+func operandCheck(num string) (int64, error) {
 
-		} else {
-			fmt.Println("Wrong operator Please try again.")
-			fmt.Print("Please enter operand :  ")
-			fmt.Scan(&num)
-		}
-	}
-	return numChecked
+	numChecked, err := strconv.ParseInt(num, 10, 64)
+
+	return numChecked, err
 }
 
-func isNumeric(s string) bool {
-	_, err := strconv.ParseInt(s, 10, 64)
-	return err == nil
-}
