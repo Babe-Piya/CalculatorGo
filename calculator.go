be2c799295af2674faa33d4159843b41c020fb4a
babe <piya.p@cdg.co.th>
2020-11-20 11:10:39 +0700

    strconv

--- calculator.go
+++ calculator.go
@@ -2,29 +2,35 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
+var first string
+var second string
+var operator string
+var result int64
+
 func main() {
-	var numFirst int
-	var operator string
-	var numSecond int
-	var result int
+
 	fmt.Print("Please enter operator :  ")
 	fmt.Scan(&operator)
 	operator = operatorCheck(operator)
+
 	fmt.Print("Please enter first number : ")
-	fmt.Scan(&numFirst)
-	// numFirstType := fmt.Sprintf("%T", numFirst)
-	// operandCheck(numFirstType)
+	fmt.Scan(&first)
+	numFirst := operandCheck(first)
+
 	fmt.Print("Please enter second number :  ")
-	fmt.Scan(&numSecond)
+	fmt.Scan(&second)
+	numSecond := operandCheck(second)
+
 	result = cal(operator, numFirst, numSecond)
 	fmt.Printf("Result : %d", result)
 
 }
 
-func cal(operator string, numFirst int, numSecond int) int {
-	var result int
+func cal(operator string, numFirst int64, numSecond int64) int64 {
+	var result int64
 
 	switch operator {
 	case "+":
@@ -53,15 +59,23 @@ func operatorCheck(operator string) string {
 	return operator
 }
 
-// func operandCheck(numFirstType string) int {
-// 	for i := 0; i < 1; {
-// 		if numFirstType != "int" {
-// 			fmt.Println("Invalid number please try again.")
-// 			fmt.Print("Please enter operator:  ")
-// 			fmt.Scan(&numFirstType)
-// 		} else {
-// 			i++
-// 		}
-// 	}
-// 	return num
-// }
+func operandCheck(num string) int64 {
+	var numChecked int64
+	for i := 0; i < 1; {
+		if isNumeric(num) {
+			numChecked, _ = strconv.ParseInt(num, 10, 64)
+			i++
+
+		} else {
+			fmt.Println("Wrong operator Please try again.")
+			fmt.Print("Please enter operand :  ")
+			fmt.Scan(&num)
+		}
+	}
+	return numChecked
+}
+
+func isNumeric(s string) bool {
+	_, err := strconv.ParseInt(s, 10, 64)
+	return err == nil
+}
